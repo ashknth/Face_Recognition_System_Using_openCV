c28d9fbaa93076b75f2eecbdef8a8800ae8e87d7
ashknth <ashoknath2076@gmail.com>
2022-06-29 03:00:27 -0400

    MY FIRST INIT

---
+++ FacialRecognition/03_face_recognition.py
@@ -0,0 +1,83 @@
+from sys import path
+import cv2
+import numpy as np
+from time import strftime
+from datetime import datetime
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath);
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+#iniciate id counter
+id = 0
+
+# names related to ids: example ==> Marcelo: id=1,  etc
+names = ['None', 'Ashok', 'Pooja', 'Nare', 'Richa', 'XYZ'] 
+
+# Initialize and start realtime video capture
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video widht
+cam.set(4, 480) # set video height
+
+# Define min window size to be recognized as a face
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+while True:
+
+    ret, img =cam.read()
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+        )
+
+    for(x,y,w,h) in faces:
+
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+
+        # Check if confidence is less them 100 ==> "0" is perfect match 
+        if (confidence < 100):
+            id = names[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+            mark_attendance(id)
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(100 - confidence))
+        
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    
+    cv2.imshow('camera',img) 
+
+    k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
+    if k == 27:
+        break
+
+
+def mark_attendance(id):
+    with open("attend.csv","r+",newline="\n") as f:
+        myDatalist=f.readlines()
+        print(myDatalist)
+        name_list=[]
+        for line in myDatalist:
+            entry=line.split((","))
+            names.append(entry[0])
+        if((id not in names)):
+            now=datetime.now()
+            d1=now.strftime("%d/%m/%Y")
+            dtString=now.strftime("%H:%M:%S")
+            f.writelines(f"\n{id}, {dtString}, {d1}, Present")
+# Do a bit of cleanup
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
